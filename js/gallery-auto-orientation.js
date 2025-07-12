43e98e39638f774cd66fcd2d927b6908f1679066
BronievM <bronievcorp@gmail.com>
2025-07-12 16:53:03 +0300

    Upd 12.07.2025

--- js/gallery-auto-orientation.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll('.gallery-item img').forEach(img => {
-    img.onload = () => {
-      const ratio = img.naturalWidth / img.naturalHeight;
-      img.parentElement.classList.add(ratio > 1 ? 'horizontal' : 'vertical');
-    };
-  });
-});
